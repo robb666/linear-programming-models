c8eb8f8c74c67835b7d25d81c2d1ff891e792037
robb <robert.patryk.grzelak@gmail.com>
2021-08-31 11:56:47 +0200

    Update ubuntu goes to excel.py

--- ubuntu goes to excel.py
+++ ubuntu goes to excel.py
@@ -8,9 +8,7 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-
 location = "/run/user/1000/gvfs/smb-share:server=192.168.1.12,share=e/Agent baza/2014 BAZA MAGRO.xlsx"
-
 
 ws = pd.read_excel(location, index_col=None, na_values=['na'], usecols="F:Z,AV")
 df = pd.DataFrame(ws)
@@ -18,21 +16,8 @@ df = pd.DataFrame(ws)
 new_header = df.iloc[1]
 df = df[2:]
 df.columns = new_header
+df = df.rename(index=lambda x: x + 2)
 
-print(df.head(100))
+kwoty = df.loc[df['Rozlicz skł. OWCA'] == 'Robert']['Przypis'].sum()
 
-
-# lodz = []
-# inne = []
-# for kod in df['kod miasto']:
-#     if str(kod).startswith('9'):
-#         lodz.append(kod)
-#     else:
-#         inne.append(kod)
-#
-# print(lodz)
-# print(inne)
-# print(len(lodz))
-# print(len(inne))
-
-
+print(kwoty)
