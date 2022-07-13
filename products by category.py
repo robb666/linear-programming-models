441388eddad10faaaaa473ec2952642bb667920c
Robb <56496611+robb666@users.noreply.github.com>
2022-07-13 12:22:57 +0200

    up

--- products by category.py
+++ products by category.py
@@ -10,7 +10,7 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-location = "/run/user/1000/gvfs/smb-share:server=192.168.1.12,share=e/Agent baza/2014 BAZA MAGRO.xlsx"
+location = "/run/user/1000/gvfs/smb-share:server=192.168.1.12,share=e/Agent baza/2014 BAZA MAGRO.xlsm"
 
 ws = pd.read_excel(location, index_col=None, na_values=['NA'], usecols="A:DB")
 df = pd.DataFrame(ws)
